5da264036bc4e59eae62f20f878326e12f7e7ce0
Tegomass <tegomassria@gmail.com>
2018-10-14 22:49:54 +0200

    Fix errors message

--- routes/users.js
+++ routes/users.js
@@ -34,7 +34,7 @@ function _getHeader(opts) {
 }
 
 function _postLogin(target, loginInfo) {
-    console.log(loginInfo);
+    console.log('_postLogin()...');
     const options = {
         method: 'POST',
         url: 'https://hr-services.fr.adp.com/ipclogin/1/loginform.fcc',
@@ -49,23 +49,36 @@ function _postLogin(target, loginInfo) {
             PASSWORD: loginInfo.password,
         },
     };
-    return new Promise(function(resolve) {
+    return new Promise(function(resolve, reject) {
         request(options, function(error, response, body) {
             if (error) throw new Error(error);
+
+            let isError = $('#error', body);
+            if (isError.length) {
+                reject({message: isError.text().trim()});
+            }
+            //console.log(response);
             //fs.writeFileSync('./_file2.html', body);
             resolve(body);
         });
     });
 }
 
-function _getPage() { //get page for time
+function _getUserInfo() { //get page for time
     const options = {
         url: config.urlSlide,
         method: 'GET',
     };
     return new Promise(function(resolve) {
         request(options, function(error, response, body) {
-            resolve($('#GMT_DATE', body).val());
+            let time = $('#GMT_DATE', body).val();
+            let name = $('.texte_nom_prenom', body).text().trim();
+            try {
+                name = name.replace('Bienvenue, ', '');
+            } catch (err) {
+                throw new Error(error);
+            }
+            resolve({name, time});
             //fs.writeFileSync('./_file4.html', body);
         });
     });
@@ -91,6 +104,7 @@ function _postPage(date) { //slide this
     return new Promise(function(resolve) {
         request(options, function(error, response, body) {
             if (error) throw new Error(error);
+            console.log(body);
             resolve(body);
         });
     });
@@ -103,48 +117,64 @@ router.get('/get', function(req, res) {
 });
 
 router.get('/', async function(req, res) {
-    if (typeof req.query.user !== 'string' ||
-        typeof req.query.password !== 'string') {
-        return res.send('error in parameters, missing "user" or "password"');
-    }
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader('Content-Type', 'application/json');
+
     console.log(req.query);
+    console.log(req.query.user.length);
+    console.log(req.query.password.length);
+    const isCredentialsMissing = typeof req.query.user !== 'string' || typeof req.query.password !== 'string' || !req.query.user.length || !req.query.password.length
+    console.log(isCredentialsMissing);
+    if (isCredentialsMissing) { //error 401 wrong parameters
+        return res.send({status: 401, message: 'error in parameters, missing "user" or "password"'});
+    }
+    console.log('why');
     let j = request.jar();
     request = request.defaults({jar: j});
 
     let header = await _getHeader();
-    console.log(header);
-    await _postLogin(header, req.query);
-    let time = await _getPage();
-    if (typeof time !== 'string') {
-        return res.send('error during slide');
+    console.log('HEADER', header);
+
+    let login = await _postLogin(header, req.query).catch(err => err);
+    if (typeof login === 'object') { //error 403 - wrong credentials
+        console.log(login);
+        return res.send({status: 403, ...login});
     }
+
+    let userInfo = await _getUserInfo();
+    console.log(userInfo);
+    if (typeof userInfo !== 'object') { //error 404 - unknown error from getting userInfo
+        return res.send({status: 404, ...userInfo});
+    }
+
+    //add a confirmation here ???
 
     let session = JSON.parse(fs.readFileSync('./session.json', 'UTF8'));
-
-    if (session.find(a => a.user === req.query.user)) {
-        return res.send('timeout in progress...');
+    if (session.find(a => a.user === req.query.user)) { //multi sliding guarding - 512 timeout in progress
+        return res.send({status: 512, message: 'Timeout in progress... Try again later'});
     }
 
-    _postPage(time); //slide here
+    // await _postPage(time); //slide here
+
 
     //clear cookies
-    console.log('tego', time);
     request = request.defaults({jar: false});
 
-
     //manage a timeout to avoid multi sliding
-    session.push(req.query);
+    session.push(req.query); //comment this to deactive the session management
     fs.writeFileSync('./session.json', JSON.stringify(session, null, 2));
 
-    let timeout = 60000 * 60; //1h
+    let timeout = 10000; //1h
+    //let timeout = 60000 * 60; //1h
+    console.log('Set timeout of', timeout, 'for', req.query.user);
     setTimeout(function() {
-        console.log('set timeout of ', timeout, 'for', req.query.user);
         let session = JSON.parse(fs.readFileSync('./session.json', 'UTF8'));
         session = session.filter(a => a.user !== req.query.user);
         fs.writeFileSync('./session.json', JSON.stringify(session, null, 2));
-        console.log('timeout cleared for ', req.query.user);
+        console.log('Timeout cleared for ', req.query.user);
     }, timeout);
-    res.send('slided for ' + time + '(GMT+2)');
+    res.send({status: 200, message: `Slided for ${userInfo.time} (GMT+2)`});
 });
 
 module.exports = router;
